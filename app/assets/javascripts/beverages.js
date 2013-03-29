678a7cbc24e34165d3882b412812b61043a2f0f3
Karthik Muthupalaniappan <kmuthupa@pathf.com>
2013-03-28 19:22:47 -0500

    Finish implementation

--- app/assets/javascripts/beverages.js
+++ app/assets/javascripts/beverages.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
   $('.select-beverage').click(function(){
+    // show processing modal
     $('#process_modal').modal({
       backdrop: 'static',
       show: true
+    });
+    
+    // post vend request
+    $.ajax({
+      type: "POST",
+      url: $(this).attr('url'),
+      data: { },
+      success: function(response) {
+        // display result
+        $('.vend_result').html(response).show();
+        //hide processing modal
+        $('#process_modal').modal('hide');
+      },
+      error: function(response) {
+        // display error
+        $('.vend_result').html('Sorry! There was a problem processing your request. Try again later.').show();
+        //hide processing modal
+        $('#process_modal').modal('hide');
+      }
     });
   });
 });
